1e1623b88e694dc1a7bd0d353fc9bc262105abef
adedotun <adedotunolawale@gmail.com>
2019-05-09 16:22:54 +0100

    c

--- config/production.js
+++ config/production.js
@@ -3,7 +3,7 @@ module.exports = production = {
         port: process.env.PORT || 5000
     },
     db: {
-        url: (process.env.MONGODB_URI) ? process.env.MONGODB_URI : '',
+        url: (process.env.MONGODB_URI) ? process.env.MONGODB_URI : 'mongodb://heroku_gxrddznw:9qariqjloov1pq9k5er3kr8hrk@ds153766.mlab.com:53766/heroku_gxrddznw',
         host: process.env.DB_HOST || 'localhost',
         port: parseInt(process.env.DB_PORT) || 27017,
         name: process.env.DB_DATABASE || 'mymappapi'        
